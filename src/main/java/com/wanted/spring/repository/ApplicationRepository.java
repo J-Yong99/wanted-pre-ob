dfe410b7f3f648a601f335a147d001fdbefe56ae
jykim-rust <alexj99@naver.com>
2023-10-04 19:55:21 +0900

    Feat: 채용공고에 대해 유저의 지원을 나타내는 Application 엔티티 구현

---
+++ src/main/java/com/wanted/spring/repository/ApplicationRepository.java
@@ -0,0 +1,7 @@
+package com.wanted.spring.repository;
+
+import com.wanted.spring.domain.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+}
