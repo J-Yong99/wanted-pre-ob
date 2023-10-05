3f7ec56454b0ece3b8c0a5505ad4a47f9fe14832
jykim-rust <alexj99@naver.com>
2023-10-05 13:05:40 +0900

    Feat: 채용공고 저장, id로 검색, 삭제 jpa메소드 추가

--- src/main/java/com/wanted/spring/repository/EmploymentNoticeRepository.java
+++ src/main/java/com/wanted/spring/repository/EmploymentNoticeRepository.java
@@ -3,5 +3,19 @@ package com.wanted.spring.repository;
 import com.wanted.spring.domain.EmploymentNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EmploymentNoticeRepository extends JpaRepository<EmploymentNotice, Long> {
+    @Override
+    <S extends EmploymentNotice> S save(S entity);
+
+    @Override
+    Optional<EmploymentNotice> findById(Long aLong);
+
+    @Override
+    void deleteById(Long aLong);
+
+    // company_id로 list<id>가져오기
+    // %검색어%로 해당하는 list<EN>가져오기
 }
