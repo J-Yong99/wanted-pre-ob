4c34847a59ce71601d4d57e47e696aa1241ad159
jykim-rust <alexj99@naver.com>
2023-10-05 15:05:08 +0900

    Feat: companyId를 이용해서 그 회사가 올린 모든 공고를 가져오는 JPA 메소드 구현

--- src/main/java/com/wanted/spring/repository/EmploymentNoticeRepositoryCustomImpl.java
+++ src/main/java/com/wanted/spring/repository/EmploymentNoticeRepositoryCustomImpl.java
@@ -1,12 +1,27 @@
 package com.wanted.spring.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.wanted.spring.domain.QEmploymentNotice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class EmploymentNoticeRepositoryCustomImpl implements EmploymentNoticeRepositoryCustom{
     private final JPAQueryFactory jpaQueryFactory;
 
+    // companyid로 해당하는 company의 모든 채용공고 id 받아오기
+    public List<Long> getAllIdByCompanyId(Long id){
+        QEmploymentNotice qEmploymentNotice = QEmploymentNotice.employmentNotice;
+
+        JPAQuery<Long> query = jpaQueryFactory.select(qEmploymentNotice.id)
+                .from(qEmploymentNotice)
+                .where(qEmploymentNotice.company.id.eq(id))
+                ;
+
+        return query.fetch();
+    }
 }
