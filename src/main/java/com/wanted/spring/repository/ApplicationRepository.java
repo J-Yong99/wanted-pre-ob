8badf7c4ea84d7acacfd30a473f759d15bce8fb7
jykim-rust <alexj99@naver.com>
2023-10-05 13:04:59 +0900

    Feat: 지원서 저장할 jpa메소드 추가

--- src/main/java/com/wanted/spring/repository/ApplicationRepository.java
+++ src/main/java/com/wanted/spring/repository/ApplicationRepository.java
@@ -4,4 +4,6 @@ import com.wanted.spring.domain.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    @Override
+    <S extends Application> S save(S entity);
 }
