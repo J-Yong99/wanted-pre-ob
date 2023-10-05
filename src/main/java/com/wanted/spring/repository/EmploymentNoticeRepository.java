ee4647b2ab28093275dc2f0915e872a6183682f4
jykim-rust <alexj99@naver.com>
2023-10-05 14:52:15 +0900

    Fix: QueryDsl사용을 위해 추가 implements

--- src/main/java/com/wanted/spring/repository/EmploymentNoticeRepository.java
+++ src/main/java/com/wanted/spring/repository/EmploymentNoticeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface EmploymentNoticeRepository extends JpaRepository<EmploymentNotice, Long> {
+public interface EmploymentNoticeRepository extends JpaRepository<EmploymentNotice, Long>, EmploymentNoticeRepositoryCustom {
     @Override
     <S extends EmploymentNotice> S save(S entity);
 
