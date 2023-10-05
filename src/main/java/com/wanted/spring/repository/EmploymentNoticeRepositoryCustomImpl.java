425de84a31e00c5a6a18f909f2acc4da8d221e1e
jykim-rust <alexj99@naver.com>
2023-10-05 15:34:37 +0900

    Feat: 검색어를 이용해서 해당 검색어가 포함되는 속성을 갖는 모든 공고(description 미포함)를 가져오는 JPA 메소드 구현

--- src/main/java/com/wanted/spring/repository/EmploymentNoticeRepositoryCustomImpl.java
+++ src/main/java/com/wanted/spring/repository/EmploymentNoticeRepositoryCustomImpl.java
@@ -1,8 +1,11 @@
 package com.wanted.spring.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.wanted.spring.domain.QEmploymentNotice;
+import com.wanted.spring.dto.EmploymentNoticeNoDetailResponseDto;
+import com.wanted.spring.dto.EmploymentNoticeYesDetailResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -22,6 +25,33 @@ public class EmploymentNoticeRepositoryCustomImpl implements EmploymentNoticeRep
                 .where(qEmploymentNotice.company.id.eq(id))
                 ;
 
+        return query.fetch();
+    }
+
+    // 검색어를 이용해서 해당 검색어가 포함되는 속성을 갖는 채용공고 찾아오기 (자세한 내용 미 포함)
+    public List<EmploymentNoticeNoDetailResponseDto> searchNoticeByWordNoDetail(String word){
+        QEmploymentNotice qEmploymentNotice = QEmploymentNotice.employmentNotice;
+
+        JPAQuery<EmploymentNoticeNoDetailResponseDto> query = jpaQueryFactory.select(Projections.constructor(
+                EmploymentNoticeNoDetailResponseDto.class,
+                qEmploymentNotice.id.as("id"),
+                qEmploymentNotice.company.name.as("companyName"),
+                qEmploymentNotice.position,
+                qEmploymentNotice.compensation,
+                qEmploymentNotice.tools,
+                qEmploymentNotice.region
+        ))
+                .from(qEmploymentNotice)
+                .where(
+                        qEmploymentNotice.company.name.likeIgnoreCase("%" + word + "%")
+                                .or(qEmploymentNotice.position.likeIgnoreCase("%" + word + "%"))
+                                .or(qEmploymentNotice.description.likeIgnoreCase("%" + word + "%"))
+                                .or(qEmploymentNotice.compensation.likeIgnoreCase("%" + word + "%"))
+                                .or(qEmploymentNotice.tools.likeIgnoreCase("%" + word + "%"))
+                                .or(qEmploymentNotice.region.likeIgnoreCase("%" + word + "%"))
+                )
+                .orderBy(qEmploymentNotice.id.asc())
+                ;
         return query.fetch();
     }
 }
