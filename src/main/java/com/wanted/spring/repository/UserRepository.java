5372b272d8fa943524bcb3ef6adf1ca03229d95a
jykim-rust <alexj99@naver.com>
2023-10-05 13:05:57 +0900

    Feat: 일반 유저 저장 jpa메소드 추가

--- src/main/java/com/wanted/spring/repository/UserRepository.java
+++ src/main/java/com/wanted/spring/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.wanted.spring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Override
+    <S extends User> S save(S entity);
 }
