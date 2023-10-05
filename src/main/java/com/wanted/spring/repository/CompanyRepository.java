af8e1ad76f54b531b669b020c9994ac499f0a83c
jykim-rust <alexj99@naver.com>
2023-10-05 13:05:09 +0900

    Feat: 회사 저장할 jpa메소드 추

--- src/main/java/com/wanted/spring/repository/CompanyRepository.java
+++ src/main/java/com/wanted/spring/repository/CompanyRepository.java
@@ -4,4 +4,6 @@ import com.wanted.spring.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    @Override
+    <S extends Company> S save(S entity);
 }
