51df196d5661404bf8bd55bb90a0320d086262b2
jykim-rust <alexj99@naver.com>
2023-10-07 08:02:13 +0900

    Feat: 지원서 등록을 위한 controller 추가

---
+++ src/main/java/com/wanted/spring/controller/ApplicationController.java
@@ -0,0 +1,31 @@
+package com.wanted.spring.controller;
+
+import com.wanted.spring.domain.Application;
+import com.wanted.spring.dto.application.ApplicationRegisterRequestDto;
+import com.wanted.spring.service.ApplicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/application")
+public class ApplicationController {
+    private final ApplicationService applicationService;
+    @Autowired
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Application> registerApplication(
+            @RequestBody ApplicationRegisterRequestDto body
+    ) {
+        return ResponseEntity.ok(applicationService.registerApplication(body));
+    }
+}
