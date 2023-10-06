b8410ccb7c0f7644210503f436ec5f6d90dc2ccf
jykim-rust <alexj99@naver.com>
2023-10-07 08:01:43 +0900

    Feat: id를 이용한 JPA메소드 추가

--- src/main/java/com/wanted/spring/repository/UserRepository.java
+++ src/main/java/com/wanted/spring/repository/UserRepository.java
@@ -3,7 +3,12 @@ package com.wanted.spring.repository;
 import com.wanted.spring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Override
+    Optional<User> findById(Long aLong);
+
     @Override
     <S extends User> S save(S entity);
 }
