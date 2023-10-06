e25cfd21fa76da9f31ec18c90d2258ac73eaf805
jykim-rust <alexj99@naver.com>
2023-10-06 11:46:55 +0900

    Feat: User 등록을 위한 service 추가

---
+++ src/main/java/com/wanted/spring/service/UserService.java
@@ -0,0 +1,31 @@
+package com.wanted.spring.service;
+
+import com.wanted.spring.domain.Company;
+import com.wanted.spring.domain.User;
+import com.wanted.spring.dto.company.CompanyRegisterRequestDto;
+import com.wanted.spring.dto.user.UserRegisterRequestDto;
+import com.wanted.spring.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(UserRegisterRequestDto body) {
+        try{
+            User user = User.builder()
+                    .age(body.getAge())
+                    .name(body.getName())
+                    .is_applied(false)
+                    .build();
+            return userRepository.save(user);
+        } catch (Exception e) {
+            System.out.println("e = " + e);
+            throw new RuntimeException(e);
+        }
+    }
+}
