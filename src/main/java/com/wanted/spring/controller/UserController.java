a37a4111027b723f044425ddd634bcfa25206839
jykim-rust <alexj99@naver.com>
2023-10-06 11:46:32 +0900

    Feat: User 등록을 위한 controller 추가

---
+++ src/main/java/com/wanted/spring/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.wanted.spring.controller;
+
+import com.wanted.spring.domain.Company;
+import com.wanted.spring.domain.User;
+import com.wanted.spring.dto.company.CompanyRegisterRequestDto;
+import com.wanted.spring.dto.user.UserRegisterRequestDto;
+import com.wanted.spring.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(
+            @RequestBody UserRegisterRequestDto body
+    ){
+        return ResponseEntity.ok(userService.registerUser(body));
+    }
+}
