cf1bbf929fde2777d966c3b897982dcf138ad75c
jykim-rust <alexj99@naver.com>
2023-10-06 12:35:37 +0900

    Feat: id를 이용해서 company 조회 JPA메소드 추가

--- src/main/java/com/wanted/spring/repository/CompanyRepository.java
+++ src/main/java/com/wanted/spring/repository/CompanyRepository.java
@@ -3,7 +3,12 @@ package com.wanted.spring.repository;
 import com.wanted.spring.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     @Override
     <S extends Company> S save(S entity);
+
+    @Override
+    Optional<Company> findById(Long aLong);
 }
