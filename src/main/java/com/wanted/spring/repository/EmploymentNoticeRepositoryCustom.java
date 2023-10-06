3e14849b7e2f4d0a49de1a390e9c424ccd82d4f3
jykim-rust <alexj99@naver.com>
2023-10-07 07:37:09 +0900

    Fix: 인터페이스와 구현체 연결안되어있던거 연결

--- src/main/java/com/wanted/spring/repository/EmploymentNoticeRepositoryCustom.java
+++ src/main/java/com/wanted/spring/repository/EmploymentNoticeRepositoryCustom.java
@@ -1,4 +1,15 @@
 package com.wanted.spring.repository;
 
+import com.wanted.spring.dto.employment_notice.EmploymentNoticeNoDetailResponseDto;
+
+import java.util.List;
+
 public interface EmploymentNoticeRepositoryCustom {
+    // companyid로 해당하는 company의 모든 채용공고 id 받아오기
+    List<Long> getAllIdByCompanyId(Long id);
+
+    // 검색어를 이용해서 해당 검색어가 포함되는 속성을 갖는 채용공고 찾아오기 (자세한 내용 미 포함)
+    List<EmploymentNoticeNoDetailResponseDto> searchNoticeByWordNoDetail(String word);
+
+    List<EmploymentNoticeNoDetailResponseDto> findAllEmploymentNotice();
 }
